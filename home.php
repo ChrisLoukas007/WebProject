6daee2097dddef560ca9b5210c7e2890da6f8d8d
Theodosis Chronopoulos <72928076+Theodoscus@users.noreply.github.com>
2022-12-17 22:38:00 +0200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,117 @@
+<?php
+
+include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css"/>
+   <script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js"></script>
+   
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+<section class="map-container">
+
+   <form action="" method="post">
+      <input type="text" name="search" placeholder="search supermarkets..." class="box">
+      <input type="submit" name="submit" value="search" class="btn">
+   </form>
+
+
+
+
+
+      <div id="map"></div>
+      
+</section>
+
+
+
+
+<section class="products">
+
+   <h1 class="title">latest products</h1>
+
+   <div class="carousel-container">
+  
+   
+   
+
+
+   
+   
+   </div>
+
+   <div class="load-more" style="margin-top: 2rem; text-align:center">
+      <a href="shop.php" class="option-btn">load more</a>
+   </div>
+
+</section>
+
+<section class="about">
+
+   <div class="flex">
+
+      <div class="image">
+         <img src="images/supermarket2.jpg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>about us</h3>
+         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit quos enim minima ipsa dicta officia corporis ratione saepe sed adipisci?</p>
+         <a href="about.php" class="btn">read more</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="home-contact">
+
+   <div class="content">
+      <h3>have any questions?</h3>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque cumque exercitationem repellendus, amet ullam voluptatibus?</p>
+      <a href="contact.php" class="white-btn">contact us</a>
+   </div>
+
+</section>
+
+
+
+
+
+<?php include 'footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+<script> var map = L.map('map').setView([38.2462420, 21.7350847], 16);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+</script>
+</body>
+</html>
